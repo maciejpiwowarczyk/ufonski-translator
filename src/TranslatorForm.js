aba1c1b681097a22d25c0982340903d833f99487
maciejpiwowarczyk <maciejpiwowarczyk@gmail.com>
2022-04-12 12:33:39 +0200

    dictionary

--- src/TranslatorForm.js
+++ src/TranslatorForm.js
@@ -5,17 +5,49 @@ import React from "react";
 class TranslatorForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { translated: "" };
+
+    this.dictionary = {
+      "a" : { char: "łą", sym: "⚡" },
+      "b" : { char: "te", sym: "☕" },
+    };
+  }
+
+  handleChange = (event) => {
+    console.log(event.target.value);
+    this.setState({ translated: this.translate(event.target.value) });
+  }
+
+  translate(orig) {
+    var translated = "";
+    var symbols = "";
+    for (var i = 0; i < orig.length; i++) 
+    {
+      var c = orig[i];
+      if (this.dictionary[c])
+      {
+        translated += this.dictionary[c].char;
+        symbols += this.dictionary[c].sym;
+      }
+      else
+      {
+        translated += c;
+        symbols += c;
+      }
+    }
+    return `${translated} (${symbols})`;
   }
 
   render() {
       return (    
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />          
           <label>
-            Enter the phrase to translate:        
+            Enter the phrase to translate:<br/>
           <input type="text" name="original" onChange={this.handleChange} />
-          </label>                
+          </label>
+          <p>In Ufoński it's: {this.state.translated}</p>
         </header>
       </div>
     );
