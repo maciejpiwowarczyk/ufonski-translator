1e395af62d4c4d489b28fe98ee4fe8bbedd07146
maciejpiwowarczyk <maciejpiwowarczyk@gmail.com>
2022-04-12 11:41:34 +0200

    changed to component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TranslatorForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+      return (    
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <label>
+            Enter the phrase to translate:        
+          <input type="text" name="original" onChange={this.handleChange} />
+          </label>                
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default TranslatorForm;
