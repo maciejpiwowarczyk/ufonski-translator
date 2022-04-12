3a63b9a7f7a014d630614ebecc2c0cda181b52f9
maciejpiwowarczyk <maciejpiwowarczyk@gmail.com>
2022-04-12 11:46:01 +0200

    renames

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import TranslatorForm from './App';
+import TranslatorForm from './TranslatorForm';
 
 ReactDOM.render(  
   <React.StrictMode>    
